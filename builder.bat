dc73b6cc87b32f09bd236371d27ec05b7b2cdcd3
Uglješa <84191191+ukicomputers@users.noreply.github.com>
2021-10-20 08:02:43 +0200

    Added builder.bat

---
+++ builder.bat
@@ -0,0 +1,29 @@
+@echo off
+title UKI COMPUTERS - Building script for Raspberry Pi Pico on C++
+echo If you have installed a Python, CMake, MinGW and GNU Arm Embeed Toolchain? [yes/no]
+set /p car=
+if %car% == yes goto start
+if %car% == no goto install
+
+:start
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" ..
+cd blink
+mingw32-make
+echo ______________________________________________
+echo Results is up. Happy Coding!
+pause
+
+:exit
+exit
+
+:install
+start https://cmake.org
+start https://python.org
+start https://www.mingw-w64.org/downloads/
+start https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads
+exit
+
+
+
